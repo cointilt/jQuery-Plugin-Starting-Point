7de41bc437dee6a15f042ed3106a62e809a0f5ed
Will Ayers <willayers.net@gmail.com>
2013-05-31 12:25:48 -0600

    Rewrote jquery plugin starting point with UMD support.

--- jquery.plugin.js
+++ jquery.plugin.js
@@ -2,32 +2,68 @@
  * jQuery Plugin Starting Point - A base jquery plugin template
  *
  * Use this template as a base to create any jQuery plugin.
+ * has UMD/AMD support
+ * also contains a super cool way to register plugins that could be forked out
  *
  * Licensed under the MIT:
  * http://www.opensource.org/licenses/mit-license.php
  *
  * @author Will Ayers ( @cointilt )
- * @version 1.1
+ * @version 2.0
  * @link https://github.com/cointilt/jQuery-Plugin-Starting-Point
 */
 
-;(function ($, document, window, undefined) {
-	$.fn.pluginName = function plugin_name_init (options) {
-		// Merge default options with passed options to create local options
-		var o = $.extend($.fn.pluginName.defaults, options);
+;(function (factory) {
+	if (typeof define == 'function' && define.amd) {
+		define(['jquery'], factory);
+	} else {
+		factory(jQuery);
+	}
+}(function ($) {
+	var pluginName = 'myPlugin',
+		App = {};
 
-		return this.each(function plugin_name_loop() {
-			// Set up basic vars
-			var $this = $(this);
-			
-			// Start Plugin here
-		});
+	App.init = function (options, el) {
+		// get options
+
+		var defaults = {};
+
+		this.options = $.extend({}, defaults, options);
+
+		// setup elements
+		this.el = el;
+		this.$el = $(el);
+
+		// return this for chaining
+		return this;
 	};
 
-	$.fn.pluginName.defaults = {
-		key: 'val',
-		customMethod: function (e, params) {
-			// Custom function
-		}
+	App.option = function (name) {
+		return this.options[name];
 	};
-}(jQuery, document, window));
+
+	if (typeof Object.create !== 'function') {
+		Object.create = function (o) {
+			function F() {}
+			F.prototype = o;
+			return new F();
+		};
+	}
+
+	$.registerPlugin = function (name, object) {
+		$.fn[name] = function (options) {
+			return this.each(function () {
+				if (!$.data(this, name)) {
+					$.data(this, name, Object.create(object).init(options, this));
+				}
+			});
+		};
+	};
+
+	// Register App as a jQuery Plugin
+	$.registerPlugin(pluginName, App);
+
+	// Return the app the be able to call specific if needed
+	return App;
+}));
+
